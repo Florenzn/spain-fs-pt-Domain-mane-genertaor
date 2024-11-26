349456bcd8d55642063ff82be169a004edec9c90
Einar <einarescobarclavijo@gmail.com>
2024-11-26 19:38:28 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let finals = [".com "];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let final of finals) {
+          domainNames.push(`${pronoun}${adj}${noun}${final}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainNames => `<li>${domainNames}</li>`)
+    .join(" ")}</ul>`;
 };
